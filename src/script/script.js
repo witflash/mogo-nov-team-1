eeff3e71ec6a8fab9b954bd54548774854f89e49
Yurii <gawrelyuk@gmail.com>
2018-01-17 21:26:25 +0200

    Fixed some bugs with trello

--- src/script/script.js
+++ src/script/script.js
@@ -102,9 +102,9 @@ $(document).ready(function(){
 
     // END SECTION 5 //
 
-    // SECTION 6 //
+    // SECTION 6 and 10//
 
-    $(".section--6 .comments").slick({ // slider initialization
+    $(".js-slider-section").slick({ // slider initialization
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
@@ -114,7 +114,7 @@ $(document).ready(function(){
         zIndex: 0,
     });
 
-    // END SECTION 6 //
+    // END SECTION 6 and 10 //
 
     // SECTION OPENMAP //
     $('.js-click-map').one('click', function () {
@@ -123,18 +123,6 @@ $(document).ready(function(){
     });
     // END SECTION OPENMAP //
 
-    // SECTION 10 //
-    $(".section--10 .comments").slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        prevArrow: '<span class="icon-arrow-left slick-arrow-prev"></span>',
-        nextArrow: '<span class="icon-arrow-right slick-arrow-next"></span>',
-        zIndex: 0,
-    });
-
-    // END SECTION 10 //
 });
 
 function loadMap() {
